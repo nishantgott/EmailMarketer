cea09c31cf32219fc2ed79efbe60f305e9f32cfa
nishantgott <nishantgk2004@gmail.com>
2024-05-16 15:34:07 +0530

    Run the campaign by using launch campaign button

--- client/src/pages/CampaignPage.js
+++ client/src/pages/CampaignPage.js
@@ -2,10 +2,27 @@ import React, { useState } from 'react'
 import Layout from '../components/Layout'
 import TemplateDropdown from '../components/TemplateDropdown'
 import UsersDropdown from '../components/UsersDropdown';
+import axios from 'axios';
 
 const CampaignPage = () => {
     const [active, setActive] = useState([]);
     const [activeRecepients, setActiveRecepients] = useState([]);
+
+    const launchCampaign = async () => {
+        console.log('button clicked');
+        // await axios.post('http://localhost:8000/email/send-this-mail', { recepient: "nishantgk2004@gmail.com", subject: "dasfgagafdg", body: "gadfdaf" });
+        try {
+            for (const a of active) {
+                for (const r of activeRecepients) {
+                    console.log('here');
+                    const send = await axios.post('http://localhost:8000/email/send-this-mail', { recepient: r, subject: a.subject, body: a.body });
+                    console.log(send);
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
 
     return (
@@ -19,7 +36,7 @@ const CampaignPage = () => {
                         <UsersDropdown activeRecepients={activeRecepients} setActiveRecepients={setActiveRecepients} />
                     </div>
                     <div className='col-md-3 mx-5 d-flex align-items-center justify-content-center'>
-                        <button type="button" className="btn btn-primary btn-lg">Launch Campaign</button>
+                        <button type="button" className="btn btn-primary btn-lg" onClick={launchCampaign} >Launch Campaign</button>
                     </div>
                 </div>
             </div>
--- controllers/emailController.js
+++ controllers/emailController.js
@@ -4,14 +4,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import emailTemplatesModel from '../models/emailtemplate.js'
 
-const mailgun = new Mailgun(formData);
-const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY || '7b20e7197bd988530337f543151e0db7-32a0fef1-75a6b3e8' });
-
 export const sendEmailController = async (req, res) => {
     try {
+        const mailgun = new Mailgun(formData);
+        const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY });
+        console.log(process.env.MAILGUN_API_KEY)
         console.log('sending email...\n');
-        mg.messages.create('sandbox7c49c1c2a8c8494abb609519a26e8641.mailgun.org', {
-            from: "Excited User <mailgun@sandbox7c49c1c2a8c8494abb609519a26e8641.mailgun.org>",
+        mg.messages.create('sandbox4cc8e2f5a61c45048562bb634ac43ae3.mailgun.org', {
+            from: "Excited User <mailgun@sandbox4cc8e2f5a61c45048562bb634ac43ae3.mailgun.org>",
             to: ["nishantgk2004@gmail.com"],
             subject: "Hello",
             text: "Testing some Mailgun awesomeness!",
@@ -28,14 +28,17 @@ export const sendEmailController = async (req, res) => {
 
 export const sendThisEmailController = async (req, res) => {
     try {
+        const mailgun = new Mailgun(formData);
+        const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY });
+        console.log(process.env.MAILGUN_API_KEY)
         const { recepient, subject, body } = req.body;
         console.log('sending email...\n');
-        mg.messages.create('sandbox7c49c1c2a8c8494abb609519a26e8641.mailgun.org', {
-            from: "Excited User <mailgun@sandbox7c49c1c2a8c8494abb609519a26e8641.mailgun.org>",
+        mg.messages.create('sandbox4cc8e2f5a61c45048562bb634ac43ae3.mailgun.org', {
+            from: "Excited User <mailgun@sandbox4cc8e2f5a61c45048562bb634ac43ae3.mailgun.org>",
             to: [recepient],
             subject: subject,
             text: body,
-            html: "<h1>Testing some Mailgun awesomeness!</h1>"
+            html: `<h1>${body}</h1>`
         })
             .then(msg => console.log(msg)) // logs response data
             .catch(err => console.log(err)); // logs any error
