3dc111eb73f2877571612b092328d05f2803dbf5
nishantgott <nishantgk2004@gmail.com>
2024-05-16 10:42:55 +0530

    created react app and set it up

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App ">
+      <Routes>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
